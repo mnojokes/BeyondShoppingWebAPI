ac8132714b6c11e15c48c05e097ddeac1abcf90b
Mantas Naujokas <mantas.naujokas@gmail.com>
2024-03-15 23:54:09 +0200

    Covered OrderService's CreateOrder method with unit tests. Updated NuGet packages.

--- tests/BeyondShopping.Application.Tests/OrderServiceTests.cs
+++ tests/BeyondShopping.Application.Tests/OrderServiceTests.cs
@@ -1,10 +1,14 @@
 using BeyondShopping.Application.Services;
 using BeyondShopping.Application.Validators;
+using BeyondShopping.Contracts;
 using BeyondShopping.Contracts.Objects;
 using BeyondShopping.Contracts.Requests;
 using BeyondShopping.Core.Exceptions;
 using BeyondShopping.Core.Interfaces;
+using BeyondShopping.Core.Models;
+using FluentAssertions;
 using Moq;
+using System.Data;
 
 namespace BeyondShopping.Application.Tests;
 
@@ -18,12 +22,15 @@ public class OrderServiceTests
 
     public OrderServiceTests()
     {
-        //Mock<IConfiguration> configurationMock = new Mock<IConfiguration>();
-        //configurationMock.Setup(x => x["UserDataRepositoryAddress"]).Returns("ConnectionString");
-
         _userRepositoryMock = new Mock<IUserRepository>();
         _orderRepositoryMock = new Mock<IOrderRepository>();
         _orderItemRepositoryMock = new Mock<IOrderItemRepository>();
+
+        _userRepositoryMock.Setup(r => r.Get(It.Is<int>(id => id >= 1 && id <= 10))).Returns((int id) => Task.FromResult<UserData?>(new UserData(id, "TestName", "TestUsername", "TestEmail")));
+        _userRepositoryMock.Setup(r => r.Get(It.Is<int>(id => id < 1 || id > 10))).Returns(Task.FromResult<UserData?>(null));
+
+        _orderRepositoryMock.Setup(r => r.OpenConnectionAndStartTransaction()).Returns(new Mock<IDbTransaction>().Object);
+        _orderRepositoryMock.Setup(r => r.Create(It.IsAny<OrderDataModel>(), It.IsAny<IDbTransaction>())).Returns(Task.FromResult(new OrderDataModel(1, 1, OrderStatus.Pending, DateTime.Now)));
 
         _orderService = new OrderService(
             _userRepositoryMock.Object,
@@ -35,10 +42,83 @@ public class OrderServiceTests
     }
 
     [Fact]
-    public async Task CreateOrder_GivenInvalidRequest_ThrowsDataValidationException()
+    public async Task CreateOrder_GivenRequestWithInvalidUserId_ThrowsDataValidationException()
     {
-        CreateOrderRequest request = new CreateOrderRequest(-1, new List<OrderItem>());
-        await Assert.ThrowsAsync<DataValidationException>(async () => await _orderService.CreateOrder(request));
+        CreateOrderRequest request = new CreateOrderRequest(-1, new List<OrderItem>() { new OrderItem(1, 1) });
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<DataValidationException>();
         _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenRequestWithEmptyItemsList_ThrowsDataValidationException()
+    {
+        CreateOrderRequest request = new CreateOrderRequest(1, new List<OrderItem>());
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<DataValidationException>();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenRequestWithInvalidItemId_ThrowsDataValidationException()
+    {
+        CreateOrderRequest request = new CreateOrderRequest(1, new List<OrderItem>() { new OrderItem(-1, 1) });
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<DataValidationException>();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenRequestWithInvalidItemQuantity_ThrowsDataValidationException()
+    {
+        CreateOrderRequest request = new CreateOrderRequest(1, new List<OrderItem>() { new OrderItem(1, 0) });
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<DataValidationException>();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenNonExistentUserId_ThrowsUserNotFoundException()
+    {
+        CreateOrderRequest request = new CreateOrderRequest(11, new List<OrderItem>() { new OrderItem(1, 1) });
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<UserNotFoundException>();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenNonExistentItemId_ThrowsItemNotFoundException()
+    {
+        CreateOrderRequest request = new CreateOrderRequest(1, new List<OrderItem>() { new OrderItem(101, 1) });
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().ThrowAsync<ItemNotFoundException>();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Never());
+    }
+
+    [Fact]
+    public async Task CreateOrder_GivenValidRequest_ReturnsOrderResponse()
+    {
+        int testUserId = 1;
+        List<OrderItem> testItems = new List<OrderItem>()
+        {
+            new OrderItem(1, 1),
+            new OrderItem(2, 2),
+            new OrderItem(3, 3)
+        };
+
+        CreateOrderRequest request = new CreateOrderRequest(testUserId, testItems);
+        Func<Task> act = async () => await _orderService.CreateOrder(request);
+
+        await act.Should().NotThrowAsync();
+        _orderRepositoryMock.Verify(r => r.OpenConnectionAndStartTransaction(), Times.Once());
+        _orderRepositoryMock.Verify(r => r.Create(It.IsAny<OrderDataModel>(), It.IsAny<IDbTransaction>()), Times.Once());
+        _orderItemRepositoryMock.Verify(r => r.Create(It.IsAny<OrderItemModel>(), It.IsAny<IDbTransaction>()), Times.Exactly(testItems.Count));
+        _orderRepositoryMock.Verify(r => r.CloseConnectionAndCommit(It.IsAny<IDbTransaction>()));
     }
 }
